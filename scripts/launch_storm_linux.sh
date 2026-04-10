d6c95d7378569b5e545fe7ca723f7697a4fc2e76
hailstone1 <hailstone1@localhost.localdomain>
2026-04-10 16:54:18 -0500

    linux patching

---
+++ scripts/launch_storm_linux.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+export PATH="/home/hailstone1/miniforge3/envs/storm/bin:/home/hailstone1/miniforge3/bin:/home/hailstone1/miniforge3/condabin:/usr/local/bin:/usr/bin:/bin"
+export CONDA_PREFIX="/home/hailstone1/miniforge3/envs/storm"
+cd "/home/hailstone1/Documents/storm"
+exec "/home/hailstone1/miniforge3/envs/storm/bin/python" main.py
